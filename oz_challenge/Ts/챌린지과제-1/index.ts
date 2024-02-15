8d51463f073d7c41389ca392603d0a430c633a21
lsh7465 <lsh746527@gmail.com>
2024-02-15 15:28:56 +0900

    .

--- oz_challenge/Ts/챌린지과제-1/index.ts
+++ oz_challenge/Ts/챌린지과제-1/index.ts
@@ -2,23 +2,24 @@
 // 큐: 선형 자료구조, FIFO (먼저 들오는 순서대로 처리)
 
 class GenericQueue<T> {
-  private items: T[] = [];.
+  private items: T[] = [];
 
   //enqueue 메서드 (큐를 데이터에 추가)
   enqueue(item: T): void {
-      this.items.push(item);
+    this.items.push(item);
   }
   //dequeue 메서드 (큐의 맨처음 데이터를 꺼냄)
-  dequeue(): T | undefined {//비어있을때 undefined 반환되기 때문에 union타입 사용
-      return this.items.shift();
+  dequeue(): T | undefined {
+    //비어있을때 undefined 반환되기 때문에 union타입 사용
+    return this.items.shift();
   }
   //peek 메서드 (큐의 맨처음 데이터를 확인)
   peek(): T | undefined {
-      return this.items[0];
+    return this.items[0];
   }
   //size 메서드 (현재 큐의 사이즈반환)
   size(): number {
-      return this.items.length;
+    return this.items.length;
   }
 }
 
@@ -34,5 +35,5 @@ console.log(stringQ.peek()); //Typescript 읽어옴, Typescript
 
 const numberQ = new GenericQueue<number>();
 
-numberQ.enqueue(10)
-numberQ.enqueue(20)
+numberQ.enqueue(10);
+numberQ.enqueue(20);
